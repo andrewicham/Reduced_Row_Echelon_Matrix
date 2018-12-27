57e0c1b4798a1f64a8da1d25a6367dcfeb7b7869
andrewicham <andrewicham@gmail.com>
2018-12-26 21:17:51 -0500

    fixed row cancelling operations

--- js/rref.js
+++ js/rref.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    
     function Row(x, y, z, divID){
     this.x = x;
     this.y = y;
@@ -121,8 +120,9 @@ $(document).ready(function(){
         return returnRows;
     };
     
-    function rowArithmetic(row1, row2, row3){
-        if((row1[0] == 1) && ((row3[0] != 0) || (row1[0] == row3[0]))){
+    //change name to column 1 negator
+    function rowNegator(row1, row2, row3){
+        if((row1[0] == 1) && (row3[0] != 0 || row1[0] == row3[0])){
             negRow1 = []; //this will create another temporary array to perfrom operations on
             negRow1[0] = (-1 * row3[0]) * row1[0]; //negates the value in position 0 of row 3 and multiplies by LCM
             negRow1[1] = (-1 * row3[0]) * row1[1];
@@ -131,14 +131,16 @@ $(document).ready(function(){
             newRow3[0] = row3[0] + negRow1[0]; //cancels out data in array position zero
             newRow3[1] = row3[1] + negRow1[1]; //adds negRow[1] to row3[1] to stay consistent
             newRow3[2] = row3[2] + negRow1[2]; // see above
-            row3 = newRow3; //sets row3 to new cancelled row
-            console.log("Row Arithmetic");
+            row3[0] = newRow3[0];
+            row3[1] = newRow3[1];
+            row3[2] = newRow3[2]; //sets row3 to new cancelled row
+            console.log("Row Negator");
             console.log("Row 1: " + row1);
             console.log("Row 2: " + row2);
             console.log("Row 3: " + row3);
-            rowArithmetic(row1, row2, row3);
+            rowNegator(row1, row2, row3);
         }
-        else if((row1[0] == 1) && ((row2[0] != 0) || row1[0] == row2[0])){
+        else if((row1[0] == 1) && (row2[0] != 0 || row1[0] == row2[0])){
             negRow1 = [];
             negRow1[0] = (-1 * row2[0]) * row1[0];
             negRow1[1] = (-1 * row2[0]) * row1[1];
@@ -147,68 +149,130 @@ $(document).ready(function(){
             newRow2[0] = row2[0] + negRow1[0];
             newRow2[1] = row2[1] + negRow1[1];
             newRow2[2] = row2[2] + negRow1[2];
-            row2 = newRow2;
-            console.log("Row Arithmetic");
+            row2[0] = newRow2[0];
+            row2[1] = newRow2[1];
+            row2[2] = newRow2[2];
+            console.log("Row Negator");
             console.log("Row 1: " + row1);
             console.log("Row 2: " + row2);
             console.log("Row 3: " + row3);
-            rowArithmetic(row1, row2, row3);
+            rowNegator(row1, row2, row3);
         }
-        
-        /*tested, this works without the recursive call*/ 
-        else if((row2[0] == 0) && (row1[0] != 0))
-        {
-            //next, we need to multiply row 1 by the value in row2[1]
-            //we are doing this using least common multiple elimination in order to avoid using fractional values
+        returnRows = [];
+        returnRows[0] = row1;
+        returnRows[1] = row2;
+        returnRows[2] = row3;
+        return returnRows;
+    }
+
+    function rowArithmetic(row1, row2, row3){
+        if((row2[0] == 0) && (row1[0] != 0) && (row1[1] != 0))
+        {   
+            console.log("Before Arithmetic: " + row2);
             multRow1 = [];
-            multRow1[0] = (row2[1] * row1[0]);
-            multRow1[1] = (row2[1] * row1[1]);
-            multRow1[2] = (row2[1] * row1[2]);
-            //now, multiply row2 by negative value of row1[1] and store in temp variable multRow2
             multRow2 = [];
-            if(multRow1[1] > 0){
-                multRow2[0] = (row1[1]) * row2[0]; //this should be true for this if statement to execute regardless, because row2[0] should equal 0
-                multRow2[1] = (row1[1]) * row2[1];
-                multRow2[2] = (row1[1]) * row2[2];
+            /* this next step is important. its important that the multiplied versions of row 1 and row 2 are 
+            of opposite sign, so that when added together, row1[1] cancels out */ 
+            if((row1[1] < 0 && row2[1] > 0) || (row1[1] > 0 && row2[1] < 0)){
+                
+                multRow1[0] = ((-1 * row2[1]) * row1[0]);
+                multRow1[1] = ((-1 * row2[1]) * row1[1]); 
+                multRow1[2] = ((-1 * row2[1]) * row1[2]);
+                multRow2[0] = (row1[1] * row2[0]);
+                multRow2[1] = (row1[1] * row2[1]);
+                multRow2[2] = (row1[1] * row2[2]);
             }
+            
             else{
-                multRow2[0] = (-1 * row1[1]) * row2[0]; //this should be true for this if statement to execute regardless, because row2[0] should equal 0
-                multRow2[1] = (-1 * row1[1]) * row2[1];
-                multRow2[2] = (-1 * row1[1]) * row2[2];
+                multRow1[0] = ((row2[1] * row1[0]));
+                multRow1[1] = ((row2[1] * row1[1])); 
+                multRow1[2] = ((row2[1] * row1[2]));
+                multRow2[0] = (row1[1] * row2[0]);
+                multRow2[1] = (row1[1] * row2[1]);
+                multRow2[2] = (row1[1] * row2[2]);
             }
+            
             row1[0] = multRow1[0] + multRow2[0];
             row1[1] = multRow1[1] + multRow2[1];
             row1[2] = multRow1[2] + multRow2[2];
-            console.log("Row Arithmetic");
+            console.log("Fixing Row 1");
             console.log("Row 1: " + row1);
             console.log("Row 2: " + row2);
             console.log("Row 3: " + row3);
-            //rowArithmetic(row1, row2, row3);
+            
         }
+        
+        if((row2[0] == 0) && (row3[1] != 0)){
+            multRow3 = [];
+            multRow2 = [];
+            
+            if((row3[1] > 0 && row2[1] > 0) || (row3[1] < 0 && row2[1] < 0)){
+                multRow3[0] = (-1 * row2[1] * row3[0]);
+                multRow3[1] = (-1 * row2[1] * row3[1]);
+                multRow3[2] = (-1 * row2[1] * row3[2]);
+                multRow2[0] = (row3[1] * row2[0]);
+                multRow2[1] = (row3[1] * row2[1]);
+                multRow2[2] = (row3[1] * row2[2]);
+            }
+            else{
+                multRow3[0] = (row2[1] * row3[0]);
+                multRow3[1] = (row2[1] * row3[1]);
+                multRow3[2] = (row2[1] * row3[2]);
+                multRow2[0] = (row3[1] * row2[0]);
+                multRow2[1] = (row3[1] * row2[1]);
+                multRow2[2] = (row3[1] * row2[2]);
+            }
+            row3[0] = multRow3[0] + multRow2[0];
+            row3[1] = multRow3[1] + multRow2[1];
+            row3[2] = multRow3[2] + multRow2[2];
+            console.log("Fixing Row 3");
+            console.log("Row 1: " + row1);
+            console.log("Row 2: " + row2);
+            console.log("Row 3: " + row3);
+            console.log(" ");
+            
+        }
+        returnRows = [];
+        returnRows[0] = row1;
+        returnRows[1] = row2;
+        returnRows[2] = row3;
+        return returnRows;
     };
 
+
     var rowDiv1 = "row1";
-    var row1obj = new Row(1, -3, 4, rowDiv1);
+    var row1obj = new Row(7, -8, 9, rowDiv1);
     var row1 = row1obj.makeArray();
     row1obj.printArray();
 
     var rowDiv2 = "row2";
-    var row2obj = new Row(3, -12, 32, rowDiv2);
+    var row2obj = new Row(1, 6, 7, rowDiv2);
     var row2 = row2obj.makeArray();
     row2obj.printArray();
 
     var rowDiv3 = "row3";
-    var row3obj = new Row(3, 1, 6, rowDiv3);
+    var row3obj = new Row(4, 5, 6, rowDiv3);
     var row3 = row3obj.makeArray();
     row3obj.printArray();
 
     cancelRows = rowCanceller(row1, row2, row3);
-    var appendedRows = rowArranger(row1, row2, row3);
-    newRow1 = appendedRows[0];
-    newRow2 = appendedRows[1];
-    newRow3 = appendedRows[2];
+
+    var arrangedRows = rowArranger(row1, row2, row3);
+    newRow1 = arrangedRows[0];
+    newRow2 = arrangedRows[1];
+    newRow3 = arrangedRows[2];
     
-    arith = rowArithmetic(newRow1, newRow2, newRow3);
-    
-    
+    var negateRows = rowNegator(newRow1, newRow2, newRow3);
+    negatedrow1 = negateRows[0];
+    negatedrow2 = negateRows[1]; 
+    negatedrow3 = negateRows[2];
+
+    var arithRows = rowArithmetic(negatedrow1, negatedrow2, negatedrow3);
+    arRow1 = arithRows[0];
+    arRow2 = arithRows[1];
+    arRow3 = arithRows[2];
+    console.log("Final Row 1: " + arRow1);
+    console.log("Final Row 2: " + arRow2);
+    console.log("Final Row 3: " + arRow3);
+     
 });
